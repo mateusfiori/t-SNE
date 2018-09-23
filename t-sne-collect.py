1feb682b55df5e996f785ced000fc96da1f66b71
Mateus Fiori <mateusfiori@Mateuss-MacBook-Pro.local>
2018-09-23 01:59:51 -0300

    Added positive and negative text dataset

--- t-sne-collect.py
+++ t-sne-collect.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 
-comments_file = open("./comments.csv", "w")
-directory = os.fsencode("/Users/mateusfiori/Main Files/Mentoria/t-SNE/aclImdb/train/unsup")
-prefix_path = "/Users/mateusfiori/Main Files/Mentoria/t-SNE/aclImdb/train/unsup"
+comments_file = open("./pos.csv", "w")
+directory = os.fsencode("/Users/mateusfiori/Main Files/Mentoria/t-SNE/aclImdb/train/pos")
+prefix_path = "/Users/mateusfiori/Main Files/Mentoria/t-SNE/aclImdb/train/pos"
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
--- t-sne.py
+++ t-sne.py
@@ -4,27 +4,31 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 #shape -> [10000, 1]
-comments = pd.read_csv("./comments.csv").values
-comments = comments[0:1000] #original size was taking too long to process
+#comments = pd.read_csv("./comments.csv").values
+comments_neg = pd.read_csv("./neg.csv").values
+comments_pos = pd.read_csv("./pos.csv").values
 
+comments = np.concatenate((comments_neg[0:1000], comments_pos[0:1000]))  # original size was taking too long to process
 
 print("Waiting for BOW")
 #Bag of Words
-vectorizer = CountVectorizer(stop_words='english', lowercase=True, max_df=0.95, min_df=0.05)
+vectorizer = CountVectorizer(stop_words='english', lowercase=True, max_df=0.95, min_df=0.01)
 X = vectorizer.fit_transform(comments.ravel())
+print(X.shape)
 print("BOW done...\n")
 
 print("Waiting for TSVD")
 #TruncatedSVD - dimensionality reduction for sparse data
-tSVD = TruncatedSVD(n_components=100)
+tSVD = TruncatedSVD(n_components=20)
 dec_x = tSVD.fit_transform(X)
 print("TSVD done...\n")
 
 print("Waiting for T-SNE...")
 #t-SNE
-X_embedded = TSNE(n_components=3, verbose=1, learning_rate=500.0).fit_transform(dec_x)
+X_embedded = TSNE(n_components=3, verbose=1, learning_rate=300.0).fit_transform(dec_x)
 print("T-SNE done...\n")
 
 #plotting
